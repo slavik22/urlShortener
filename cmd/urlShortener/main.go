e3713f6c4c940db8b604a72512110e5b10cbdc26
Yaroslav Fedyna <fedynaslava@gmail.com>
2023-07-25 11:33:13 +0300

    SqlLite inited

--- cmd/urlShortener/main.go
+++ cmd/urlShortener/main.go
@@ -1,12 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"github.com/slavik22/urlShortener/internal/config"
+	"github.com/slavik22/urlShortener/internal/lib/logger/sl"
+	"github.com/slavik22/urlShortener/internal/storage/sqlite"
+	"golang.org/x/exp/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting logging info")
+	log.Debug("Starting logging debug")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+
+	if err != nil {
+		log.Error("Failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
